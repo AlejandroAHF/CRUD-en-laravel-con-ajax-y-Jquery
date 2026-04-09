fd12dd301f5d029f929e255006a58c34b0b21df3
AlejandroAHF <aleherfernandez@gmail.com>
2026-04-08 20:56:49 -0600

    relaciones en los modeos

--- app/Models/VentaCatalogoCliente.php
+++ app/Models/VentaCatalogoCliente.php
@@ -29,4 +29,41 @@ class VentaCatalogoCliente extends Model
         'cod_municipio', 
         'descripcion_adicional'
     ];
+
+    // Relaciones Eloquent
+
+    public function tipoCliente()
+    {
+        return $this->belongsTo(TipoCliente::class, 'tipo_cliente', 'id');
+    }
+
+    public function tipoContribuyente()
+    {
+        return $this->belongsTo(TipoContribuyentes::class, 'fk_id_tipo_contribuyente', 'id_tipo_contribuyente');
+    }
+
+    public function actividadEconomica()
+    {
+        return $this->belongsTo(ActividadesEconomicas::class, 'cod_actividad_economica', 'id_actividad_economica');
+    }
+
+    public function tipoDocumento()
+    {
+        return $this->belongsTo(TipoDocumentos::class, 'cod_tipo_documento', 'id_tipo_documento');
+    }
+
+    public function pais()
+    {
+        return $this->belongsTo(Pises::class, 'fk_id_pais', 'id');
+    }
+
+    public function departamento()
+    {
+        return $this->belongsTo(Departamentos::class, 'cod_departamento', 'id_departamento');
+    }
+
+    public function municipio()
+    {
+        return $this->belongsTo(Municipios::class, 'cod_municipio', 'id_municipio');
+    }
 }
